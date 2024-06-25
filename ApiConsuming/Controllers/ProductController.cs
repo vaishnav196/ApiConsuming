6a50067f1d2bca30180ffd38fb5da32abb26389b
vaishnav196 <vaish00721@gmail.com>
2024-06-25 17:33:33 +0530

    .

--- ApiConsuming/Controllers/ProductController.cs
+++ ApiConsuming/Controllers/ProductController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Net.Security;
+using System.Text;
 
 namespace ApiConsuming.Controllers
 {
@@ -33,5 +34,75 @@ namespace ApiConsuming.Controllers
             }
             return View(products);
         }
+
+
+
+     
+        public IActionResult AddProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddProduct(Product p)
+        {
+            string url = "https://localhost:7000/api/Product/AddProduct/";
+            var jsondata = JsonConvert.SerializeObject(p);
+            StringContent content = new StringContent(jsondata, Encoding.UTF8, "application/json");
+            HttpResponseMessage respone = client.PostAsync(url, content).Result;
+            if (respone.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View();
+
+        }
+
+
+       
+
+        public IActionResult DeleteProduct(int id)
+        {
+            string url = "https://localhost:7000/api/Product/DeleteProd/";
+            HttpResponseMessage response = client.DeleteAsync(url+id).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult EditProduct(int id)
+        {
+            Product prod = new Product();
+            string url = "https://localhost:7000/api/Product/GetProdById/";
+            HttpResponseMessage response = client.GetAsync(url + id).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string jsondata = response.Content.ReadAsStringAsync().Result;
+                var obj = JsonConvert.DeserializeObject<Product>(jsondata);
+                if (obj != null)
+                {
+                    prod = obj;
+                }
+            }
+            return View(prod);
+        }
+
+        [HttpPost]
+        public IActionResult EditProduct(Product p)
+        {
+            string url = "https://localhost:7000/api/Product/UpdateProd/";
+            var jsondata = JsonConvert.SerializeObject(p);
+            StringContent content = new StringContent(jsondata, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = client.PutAsync(url, content).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }
+
