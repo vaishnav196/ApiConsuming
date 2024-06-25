26a51fe40cd0e77f4a0cbbce1f4b7cae39164cf3
vaishnav196 <vaish00721@gmail.com>
2024-06-25 15:50:55 +0530

    Add project files.

---
+++ ApiConsuming/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using ApiConsuming.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Security;
+
+namespace ApiConsuming.Controllers
+{
+    public class ProductController : Controller
+    {   HttpClient client; 
+        public ProductController()
+            //this is for Handling SSL ceertificate errors
+        {       HttpClientHandler handler = new HttpClientHandler();
+            handler.ServerCertificateCustomValidationCallback=(sender,cert,chain,SslPolicyErrors) => { return true; };
+            client = new HttpClient(handler);  
+        }
+
+        string url;
+        public IActionResult Index()
+        {
+            string url = "https://localhost:7000/GetAllProduct/";
+            List<Product> products = new List<Product>();   
+            HttpResponseMessage response=client.GetAsync(url).Result;
+            if(response.IsSuccessStatusCode)
+            {
+                string jsondata=response.Content.ReadAsStringAsync().Result;
+                var obj=JsonConvert.DeserializeObject<List<Product>>(jsondata);
+                if(obj != null)
+                {
+                    products = obj;
+                    //products.Add(obj);
+                   
+                }
+            }
+            return View(products);
+        }
+    }
+}
---
+++ ApiConsuming/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace ApiConsuming.Models
+{
+    public class Product
+    {
+       
+            public int id { get; set; }
+            public string pname { get; set; }
+            public string pcat { get; set; }
+            public string price { get; set; }
+     
+    }
+}
